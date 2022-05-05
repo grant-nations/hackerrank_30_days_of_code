dec055db0ec7eb2b52a1760f43bcedc2b187f34a
GRANT NATIONS <u1247784@umail.utah.edu>
2022-05-05 12:35:28 -0600

    initial commit

---
+++ src/challenges/Day0.java
@@ -0,0 +1,14 @@
+package challenges;
+
+import java.util.Scanner;
+
+public class Day0 {
+
+    public static void main(String[] args) {
+
+	Scanner in = new Scanner(System.in);
+    String inputString = in.nextLine();
+    System.out.println("Hello, World.");
+    System.out.println(inputString);
+    }
+}
